try to fix numerical issues with logistic fit

fit_logistic_default.py
@@ -6,13 +6,13 @@ from scipy.optimize import minimize
 def nll(theta, X, y):
     z = np.insert(X, 0, 1, 1).dot(theta)
     yz = z*y
-    log1pexpz = np.log1p(np.exp(z))
+    log1pexpz = np.logaddexp(z, np.zeros((X.shape[0])))
     return np.sum(log1pexpz - yz)
 
 datafile = sys.argv[1]
 df = pd.read_csv(datafile)
-X = default[["balance", "income"]].values
-y = np.asarray([{"Yes": 1., "No": 0.}[x] for x in default["default"]])
+X = df[["balance", "income"]].values
+y = np.asarray([{"Yes": 1., "No": 0.}[x] for x in df["default"]])
 
 m = minimize(nll, np.random.rand(3,), args=(X, y), method="L-BFGS-B")
 